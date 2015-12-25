b39da6efebef52a9fbb10859e9e5107c9c97873d
Conrad Storz <conradstorz@gmail.com>
2015-12-25 16:27:08 -0500

    gave up on first base of code after closer examination

--- Morse_Code.py
+++ Morse_Code.py
@@ -1,29 +1,70 @@
 #!/usr/bin/env python
 
+import os
 import string
 
-letters = [('A',".-"),   ('B',"-..."), ('C',"-.-."), ('D',"-.."), ('E',"."),
-           ('F',"..-."), ('G',"--."),  ('H',"...."), ('I',".."),  ('J',".---"),
-           ('K',"-.-"),  ('L',".-.."), ('M',"--"),   ('N',"-."),  ('O',"---"),
-           ('P',".--."), ('Q',"--.-"), ('R',".-."),  ('S',"..."), ('T',"-"),
-           ('U',"..-"),  ('V',"...-"), ('W',".--"),  ('X',"-..-"),('Y',"-.--"),
-           ('Z',"--..")]
+morse_char_dict = {
+        'A': '.-',     'B': '-...',   'C': '-.-.', 
+        'D': '-..',    'E': '.',      'F': '..-.',
+        'G': '--.',    'H': '....',   'I': '..',
+        'J': '.---',   'K': '-.-',    'L': '.-..',
+        'M': '--',     'N': '-.',     'O': '---',
+        'P': '.--.',   'Q': '--.-',   'R': '.-.',
+        'S': '...',    'T': '-',      'U': '..-',
+        'V': '...-',   'W': '.--',    'X': '-..-',
+        'Y': '-.--',   'Z': '--..',
+        
+        '0': '-----',  '1': '.----',  '2': '..---',
+        '3': '...--',  '4': '....-',  '5': '.....',
+        '6': '-....',  '7': '--...',  '8': '---..',
+        '9': '----.' 
+        }
 
-def decode(input):
-    if input == "" :
-        return [""]
-    else:
-        return [ letter + remaining
-                 for letter, code in letters if input.startswith(code)
-                 for remaining in decode(input[len(code):]) ]
+# create an inverse dictionary
+morse_code_dict = {v: k for k, v in morse_char_dict.items()}
 
-# Some Testing code
-def test(s, code):
-    if s in decode(code):
-        print code + " can be decoded as " + s
-    else:
-        print code + " can NOT be decoded as " + s
+sentence_break = '       '  #seven spaces
+word_break = '   '   #three spaces
+letter_break = '  '   #two spaces
 
-test("SOFIA", "...---..-....-")
-test("SOPHIA", "...---..-....-")
-test("EUGENIA", "...---..-....-")
+def decode(morse):
+    output = ''
+    sentences = morse.split(sentence_break)
+    for sentence in sentences:
+        words = sentence.split(word_break)
+        for word in words:
+            characters = word.split(letter_break)
+            for character in characters:
+                if morse_code_dict.has_key(character):
+                    output += morse_code_dict[character]
+            output += ' '
+        #output += '. '
+    return output
+
+
+def encode(message):
+    output = ''
+    for character in message:
+        if morse_char_dict.has_key(character.upper()):
+            output += morse_char_dict[character.upper()]
+            output += letter_break
+    return output
+
+def test_string_to_Morse():
+    test_list = [
+                ("Sofia ", "...  ---  ..-.  ..  .-"),
+                ("SOPHIA ", "...  ---  .--.  ....  ..  .-"),
+                ("EUGENIA ", ".  ..-  --.  .  -.  ..  .-")]
+    for text, code in test_list:
+        print 'encode: ', text
+        print encode(text)
+        print 'decode: ', code
+        print decode(code)
+        assert decode(code) == text.upper()
+
+
+if __name__ == '__main__':
+    status = decode(sys[0], result)
+    if status:
+        print result
+    sys.exit(status)
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Morse-Code
 Python code to play with string manipulation and encode/decode Morse Code strings
+
+I first proposed this as a project to share with the SIG DerbyPy on 12/22/2015
+
+The proposal is to develop Python code following TDD principals that will take strings
+and encode/decode to/from Morse Code. 
+
+As a second goal, the ability to encode/decode Morse Code
+to/from audio files would be developed.
+
+This is a learning experience. There are many sources around the Internet providing these services.
+The purpose of this exercise is to improve coding skills not just find the answer someone else has
+already worked out.
+
